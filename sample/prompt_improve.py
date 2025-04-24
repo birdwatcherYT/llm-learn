c0f3433036c34a8200a78216618d229a7426a145
birdwatcherYT <aabcehmu.362880@hotmail.co.jp>
2025-04-24 09:03:05 +0900

    改良

--- sample/prompt_improve.py
+++ sample/prompt_improve.py
@@ -9,6 +9,7 @@ from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 from langchain_core.output_parsers import StrOutputParser
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+llm_strong = ChatGoogleGenerativeAI(model="gemini-2.5-flash-preview-04-17")
 
 # 応答生成
 response_prompt = PromptTemplate.from_template(
@@ -43,7 +44,7 @@ improve_prompt = PromptTemplate.from_template(
 与えられた**プロンプト**と**対話履歴**を見て、対話履歴のaiによる出力が**フィードバック**を実現するようにプロンプトを修正してください。
 フィードバックはプロンプトの出力結果に対するもので、プロンプト自体に対するものではありません。
 対話履歴の出力を実現したいわけではありません。
-修正後のプロンプトだけを出力して、余計な説明は付与しないでください。あなたの出力結果がそのまま改善後のプロンプトとして使われます。
+修正後のプロンプトだけを出力して、余計な説明は禁止します。あなたの出力結果がそのまま改善後のプロンプトとして使われます。
 
 # 対話履歴
 ```json
@@ -57,7 +58,7 @@ improve_prompt = PromptTemplate.from_template(
 {prompt}
 """
 )
-improve_chain = improve_prompt | llm | StrOutputParser()
+improve_chain = improve_prompt | llm_strong | StrOutputParser()
 
 # 改善したいプロンプト
 prompt = """あなたはAIです。
@@ -89,9 +90,9 @@ while True:
     elif feedback == "reset":
         history.clear()
         continue
-    print(">" * 40)
+    print(">" * 20, "before", ">" * 20)
     print(prompt)
-    print("<" * 40)
+    print("<" * 48)
     feedback_history.append(feedback)
     print("feedback_history:", feedback_history)
     history_str = json.dumps(history, ensure_ascii=False, indent=1)
@@ -104,11 +105,11 @@ while True:
                 "chat_history": history_str,
             }
         )
-        print(">" * 40)
+        print(">" * 20, "after", ">" * 20)
         print(new_prompt)
-        print("<" * 40)
-        ok_ng = input("ok?ng?(ok以外はng扱い): ")
-        if ok_ng == "ok":
+        print("<" * 47)
+        ok_ng = input("これでよい？(y/nで応答。y以外はn扱い): ")
+        if ok_ng == "y":
             prompt = new_prompt
             break
     # 対話履歴が残ってると過去の生成結果に影響を受けるのでリセットしておく
